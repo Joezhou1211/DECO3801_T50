c577d4c7168759607c762ffea47873fdc9bd85ac
joe <joe.zhou666666@gamil.com>
2024-10-18 11:36:14 +1000

    renew files

--- backend/utils/CSV_to_Json.py
+++ backend/utils/CSV_to_Json.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # read the data
-file = '../../../../../DECO/data/processed/final_data(old).csv'  # path to the CSV file
+file = '../../../../../DECO/data/processed/final_data(old).csv'
 df = pd.read_csv(file)
 
 # to JSON
 json_records = df.to_dict(orient='records')
 
 # save the JSON data
-with open('../../../../../DECO/data/processed/final_data(full).json', 'w') as f:  # path to the JSON file
+with open('../../../../../DECO/data/processed/final_data(full).json', 'w') as f:
     import json
     json.dump(json_records, f)
